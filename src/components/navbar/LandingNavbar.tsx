7a99ea59db1317fb30356047ff0a8502a70ed13c
kiritoEM <johankirito64@gmail.com>
2024-05-04 15:38:19 +0200

    add route

--- src/components/navbar/LandingNavbar.tsx
+++ src/components/navbar/LandingNavbar.tsx
@@ -3,9 +3,14 @@ import { useNav } from "../../hooks/useNav";
 const LandingNavbar = (): JSX.Element => {
   const { openMenu } = useNav();
   return (
-    <nav
-      className={`landing-navbar ${openMenu && "open-nav-responsive"}`}
-    ></nav>
+    <nav className={`landing-navbar ${openMenu && "open-nav-responsive"}`}>
+      {/* section1 */}
+      <div className="nav-section1">
+        <ul>
+          
+        </ul>
+      </div>
+    </nav>
   );
 };
 
