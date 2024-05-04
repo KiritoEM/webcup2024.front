1fefff00ff32f7f32bd81866d6164d05a07df59e
herifanantenana <herifanantenana17@gmail.com>
2024-05-04 17:33:20 +0300

    add hero

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,12 @@
 import LandingLayout from "../components/layouts/LandingLayout";
+import LandingHero from "../components/sections/landing/LandingHero";
 
 const Landing = (): JSX.Element => {
   return (
     <section className="landing">
       <LandingLayout>
-        <h1></h1>
+        <h1>hello world</h1>
+        <LandingHero />
       </LandingLayout>
     </section>
   );
