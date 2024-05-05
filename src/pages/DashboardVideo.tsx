12efca2099538d2cf285f1be3dbaf65c33652246
kiritoEM <johankirito64@gmail.com>
2024-05-05 03:39:38 +0200

    add room to message

--- src/pages/DashboardVideo.tsx
+++ src/pages/DashboardVideo.tsx
@@ -1,16 +1,17 @@
 import DashboardHeader from "../components/headers/DashboardHeader";
 import Dashboardlayout from "../components/layouts/Dashboardlayout";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const DashboardVideo = (): JSX.Element => {
   const liveRoomRef = useRef<HTMLDivElement>(null);
+  const [show, setShow] = useState<boolean>(false);
   const roomName = "Trimobe-room";
 
   const liveRoom = async (element: HTMLDivElement | null) => {
     if (element) {
-      const appID = 1693363613;
-      const serverSecret = "ce96da9a664a2b944a439e1e05cbc310";
+      const appID = parseInt(import.meta.env.VITE_APP_ID);
+      const serverSecret = import.meta.env.VITE_SERVER_SECRET;
       const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
         appID,
         serverSecret,
@@ -38,7 +39,48 @@ const DashboardVideo = (): JSX.Element => {
       <Dashboardlayout>
         <div className="video-content">
           <DashboardHeader />
-          <div className="live-room" ref={liveRoomRef}></div>
+          {show && <div className="live-room" ref={liveRoomRef}></div>}
+          <div className="room-container">
+            <div className="room-list">
+              <h3>Room</h3>
+
+              <div className="list">
+                <div className="room">
+                  <div className="user">
+                    <img src="/avatar.png" alt="" />
+                    <p>Relation amoureuse</p>
+                  </div>
+                  <div className="date">
+                    <p>19:54</p>
+                  </div>
+                </div>
+                <div className="room">
+                  <div className="user">
+                    <img src="/avatar1.png" alt="" />
+                    <p>Travail</p>
+                  </div>
+                  <div className="date">
+                    <p>20:00</p>
+                  </div>
+                </div>
+                <div className="room">
+                  <div className="user">
+                    <img src="/avatar2.png" alt="" />
+                    <p>Depression</p>
+                  </div>
+                  <div className="date">
+                    <p>20:50</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="select-room">
+              <div className="content">
+                <img src="/room.png" alt="" />
+                <p>Sélectionner un Room pour continuer</p>
+              </div>
+            </div>
+          </div>
         </div>
       </Dashboardlayout>
     </section>
